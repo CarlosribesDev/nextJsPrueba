a49ea16f9212f479ac61be3402da5ddc7a6e20f5
cribesdev <carlosribes.dev@gmail.com>
2023-07-09 05:01:15 +0200

    añadir botón y auto formato

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,5 +1,29 @@
-export default function PostPage () {
+import { LikeButton } from './[id]/LikeButton'
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+const fetchPosts = (): Promise<Post[]> => {
+  const url = 'https://jsonplaceholder.typicode.com/posts'
+  return fetch(url).then(res => res.json())
+}
+
+export default async function PostPage () {
+  const posts = await fetchPosts()
+
   return (
-    <h1>lista de post</h1>
+    <section>
+      {posts.map(post => (
+        <article key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+          <LikeButton id={post.id} />
+        </article>
+      ))}
+    </section>
   )
 }
